43705fcc2a2d5cdd8922b1a96f52afb2a674c880
储天航 <1193983801@qq.com>
2022-11-10 14:10:37 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: 储天航 1193983801@qq.com
+ * @Date: 2022-11-10 13:54:16
+ * @LastEditors: 储天航 1193983801@qq.com
+ * @LastEditTime: 2022-11-10 14:00:43
+ * @FilePath: \trent-blog\docs\.vuepress\config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineUserConfig } from 'vuepress'
+// import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+})
