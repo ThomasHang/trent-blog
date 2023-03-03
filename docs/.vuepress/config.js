a9d4048088f7352811f8f7ea87ee5525b73bd19d
储天航 <1193983801@qq.com>
2023-03-03 17:40:53 +0800

    🎉 init(初始化): init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,32 +2,12 @@
  * @Author: 储天航 1193983801@qq.com
  * @Date: 2022-11-10 13:54:16
  * @LastEditors: 储天航 1193983801@qq.com
- * @LastEditTime: 2023-03-03 13:51:22
+ * @LastEditTime: 2023-03-03 15:58:12
  * @FilePath: \trent-blog\docs\.vuepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 import { defineUserConfig, defaultTheme } from "vuepress";
-
-// export default defineUserConfig({
-//   lang: "zh-CN",
-//
-//   // theme: defaultTheme({
-//   //   // 默认主题配置
-//   //   navbar: [
-//   //     {
-//   //       text: "首页",
-//   //       link: "/",
-//   //     },
-//   //   ],
-//   //   sidebar: [
-//   //     {
-//   //       text: "home",
-//   //       link: "/home",
-//   //     },
-//   //   ],
-//   // }),
-// });
 
 module.exports = {
   title: "你好， VuePress ！",
@@ -36,10 +16,6 @@ module.exports = {
   theme: defaultTheme({
     // 默认主题配置
     navbar: [
-      // {
-      //   text: "首页",
-      //   link: "/",
-      // },
       {
         text: "首页",
         link: "/",
@@ -56,13 +32,10 @@ module.exports = {
         text: "工作",
         link: "/work/",
       },
-      // NavbarGroup
       {
         text: "Group",
         children: ["/group/foo.md", "/group/bar.md"],
       },
-      // 字符串 - 页面文件路径
-      // "/bar/README.md",
     ],
     sidebar: {
       "/essay/": [
@@ -71,7 +44,13 @@ module.exports = {
           children: ["/essay/title1.md", "/essay/title2.md"],
         },
       ],
+      "/work/": [
+        {
+          children: [
+            { text: "react-window虚拟渲染", link: "/work/record/react-window" },
+          ],
+        },
+      ],
     },
-    // SidebarItem
   }),
 };
