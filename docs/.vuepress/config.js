3aee8638394bc7e1d4317a15ededb2b15222c89d
ThomasHang <11939838031@qq.com>
2023-03-07 08:23:41 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,57 +1,61 @@
 /*
  * @Author: 储天航 1193983801@qq.com
  * @Date: 2022-11-10 13:54:16
- * @LastEditors: 储天航 1193983801@qq.com
- * @LastEditTime: 2023-03-06 17:41:01
+ * @LastEditors: ThomasHang 11939838031@qq.com
+ * @LastEditTime: 2023-03-07 00:10:19
  * @FilePath: \trent-blog\docs\.vuepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-import { defineUserConfig, defaultTheme } from "vuepress";
+import { defineUserConfig, defaultTheme } from 'vuepress';
 // import { backToTopPlugin } from "@vuepress/plugin-back-to-top";
 // import {ss} from "vuepress-plugin-mermaidjs"
 
 module.exports = {
-  title: "你好， VuePress ！",
-  description: "Hello, my friend!",
-  base: "/trent-blog/",
-  plugins: ["@vuepress/plugin-back-to-top", "vuepress-plugin-mermaidjs"],
+  title: '加油， 打工人 ！',
+  description: 'Hello, my friend!',
+  base: '/trent-blog/',
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    'vuepress-plugin-mermaidjs',
+    '@vuepress/plugin-medium-zoom',
+  ],
   theme: defaultTheme({
     // 默认主题配置
     navbar: [
       {
-        text: "首页",
-        link: "/",
+        text: '首页',
+        link: '/',
       },
       {
-        text: "关于",
-        link: "/about/",
+        text: '关于',
+        link: '/about/',
       },
       {
-        text: "随笔",
-        link: "/essay/",
+        text: '随笔',
+        link: '/essay/',
       },
       {
-        text: "工作",
-        link: "/work/",
+        text: '工作',
+        link: '/work/',
       },
       {
-        text: "Group",
-        children: ["/group/foo.md", "/group/bar.md"],
+        text: 'Group',
+        children: ['/group/foo.md', '/group/bar.md'],
       },
     ],
     sidebar: {
-      "/essay/": [
+      '/essay/': [
         {
-          text: "随便",
-          children: ["/essay/title1.md", "/essay/title2.md"],
+          text: '随便',
+          children: ['/essay/title1.md', '/essay/title2.md'],
         },
       ],
-      "/work/": [
+      '/work/': [
         {
           children: [
-            { text: "react-window虚拟渲染", link: "/work/record/react-window" },
-            { text: "redux", link: "/work/learn/redux.md" },
+            { text: 'react-window虚拟渲染', link: '/work/record/react-window' },
+            { text: 'redux', link: '/work/learn/redux.md' },
           ],
         },
       ],
--- docs/work/learn/redux.md
+++ docs/work/learn/redux.md
@@ -1,8 +1,8 @@
 <!--
  * @Author: 储天航 1193983801@qq.com
  * @Date: 2023-03-06 16:08:31
- * @LastEditors: 储天航 1193983801@qq.com
- * @LastEditTime: 2023-03-06 17:42:05
+ * @LastEditors: ThomasHang 11939838031@qq.com
+ * @LastEditTime: 2023-03-06 23:52:25
  * @FilePath: \trent-blog\docs\work\learn\redux.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -57,7 +57,9 @@
 - Selectors : Selectors are functions that know how to extract specific pieces of information from a store state value.(如何从存储状态值中提取特定信息的函数。)
 
 
-```mermaid
+# 流程图
+
+``` mermaid
 sequenceDiagram
 Alice->John: Hello John, how are you?
 loop every minute
