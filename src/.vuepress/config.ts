0b02ef956261d19779b10a7e3aefdb4dc01dcb93
ThomasHang <11939838031@qq.com>
2024-09-28 22:51:36 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,9 @@
 import { defineUserConfig } from "vuepress";
+
 import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-  dest: "dist",
 
   lang: "zh-CN",
   title: "Trent",
