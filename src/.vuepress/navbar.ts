f24f1129cc14ea643fb080c93c7aae85bc6d73ad
ThomasHang <11939838031@qq.com>
2024-09-28 23:53:27 +0800

    update

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,13 +1,13 @@
-import { navbar } from "vuepress-theme-hope";
+import { navbar } from 'vuepress-theme-hope';
 
 export default navbar([
-  "/",
+  '/',
   // "/demo/",
   {
-    text: "代码笔记",
-    icon: "bijiben",
+    text: '代码笔记',
+    icon: 'bijiben',
     children: [
-      { text: "代码笔记", icon: "code", link: "/code/" },
+      { text: '代码笔记', icon: 'code', link: '/code/' },
       // {
       //   text: "javascript",
       //   icon: "code",
@@ -16,91 +16,47 @@ export default navbar([
     ],
   },
   {
-    text: "软件教程",
-    icon: "soft",
-    prefix: "/software/",
+    text: '软件教程',
+    icon: 'soft',
+    prefix: '/software/',
     children: [
       {
-        text: "软件教程",
-        icon: "software",
-        link: "",
-        activeMatch: "^/software/$",
-      },
-      // "vscode/",
-      // "git/",
-      // "comsol/",
-    ],
-  },
-  {
-    text: "健身",
-    icon: "gym",
-    prefix: "/gym/",
-    children: [
-      {
-        text: "健身",
-        icon: "gym",
-        link: "",
-        activeMatch: "^/gym/$",
+        text: '软件教程',
+        icon: 'software',
+        link: '',
+        activeMatch: '^/software/$',
       },
     ],
   },
   {
-    text: "随笔",
-    icon: "read",
-    prefix: "/read/",
+    text: '健身',
+    icon: 'gym',
+    prefix: '/gym/',
     children: [
       {
-        text: "随笔",
-        icon: "read",
-        link: "",
-        activeMatch: "^/read/$",
+        text: '健身',
+        icon: 'gym',
+        link: '',
+        activeMatch: '^/gym/$',
       },
     ],
   },
-  // {
-  //   text: "博文",
-  //   icon: "pen-to-square",
-  //   prefix: "/posts/",
-  //   children: [
-  //     {
-  //       text: "苹果",
-  //       icon: "pen-to-square",
-  //       prefix: "apple/",
-  //       children: [
-  //         { text: "苹果1", icon: "pen-to-square", link: "1" },
-  //         { text: "苹果2", icon: "pen-to-square", link: "2" },
-  //         "3",
-  //         // "4",
-  //       ],
-  //     },
-  //     {
-  //       text: "香蕉",
-  //       icon: "pen-to-square",
-  //       prefix: "banana/",
-  //       children: [
-  //         {
-  //           text: "香蕉 1",
-  //           icon: "pen-to-square",
-  //           link: "1",
-  //         },
-  //         {
-  //           text: "香蕉 2",
-  //           icon: "pen-to-square",
-  //           link: "2",
-  //         },
-  //         "3",
-  //         "4",
-  //       ],
-  //     },
-  //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-  //     { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-  //     "tomato",
-  //     "strawberry",
-  //   ],
-  // },
-  // {
-  //   text: "V2 文档",
-  //   icon: "book",
-  //   link: "https://theme-hope.vuejs.press/zh/",
-  // },
+  {
+    text: '随笔',
+    icon: 'read',
+    prefix: '/read/',
+    children: [
+      {
+        text: '随笔',
+        icon: 'read',
+        link: '',
+        activeMatch: '^/read/$',
+      },
+    ],
+  },
+  {
+    text: 'todo',
+    icon: 'todo',
+    link: '/todo/',
+  },
 ]);
--- src/todo/README.md
+++ src/todo/README.md
@@ -1,5 +1,21 @@
+---
+title: 信息收集
+---
+
 [web3.0](https://www.wtf.academy/)
 
 https://m.okjike.com/originalPosts/66f5f5656663fa08b5e535ec
 
 https://www.bilibili.com/video/BV17dxEeBEqU/?buvid=ad4898b17bf8b490c320d45bec68118c&from_spmid=united.player-video-detail.0.0&is_story_h5=false&mid=9%2F6Gg%2Fhr6u5NGuQO8DKEFQ%3D%3D&p=1&plat_id=116&share_from=ugc&share_medium=iphone&share_plat=ios&share_session_id=4CB57EAD-236D-487E-8682-FC0529ECB3FF&share_source=COPY&share_tag=s_i&spmid=united.player-video-detail.0.0&timestamp=1727394965&unique_k=YF6oibA&up_id=14097567&vd_source=0674304a3d1219c3ddfcf9bda7eb54e9
+
+官网制作框架
+
+- [Gatsbyjs](https://www.gatsbyjs.com/)
+- [Astro](https://astro.build/)
+
+动效 (部分收费)
+
+- [magic ui](https://magicui.design/)
+
+- [nextjs](https://nextjs.org/)
+- [vercel ai template](https://vercel.com/templates/ai)
