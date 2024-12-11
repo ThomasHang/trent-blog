214a4448a171bcaca598a5af02feb4b3c04a9294
储天航 <18136106648@163.com>
2024-12-11 14:40:25 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/trent-blog/",
 
   lang: "zh-CN",
   title: "Trent",
